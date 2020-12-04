1ec227196f39dcfa583a1573a193911de58a6cd1
Patrick1993G <49478732+Patrick1993G@users.noreply.github.com>
2020-12-04 17:30:29 +0100

    adding register

--- Solution3/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
+++ Solution3/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
@@ -26,5 +26,6 @@ namespace ShoppingCart.Data.Context
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Member> Members { get; set; }
     }
 }
--- Solution3/ShoppingCart.IOC/DependancyContainer.cs
+++ Solution3/ShoppingCart.IOC/DependancyContainer.cs
@@ -24,6 +24,9 @@ namespace ShoppingCart.IOC
             services.AddScoped<ICategoriesRepository, CategoriesRepository>();
             services.AddScoped<ICategoriesService, CategoriesService>();
 
+            services.AddScoped<IMembersRepository, MembersRepository>();
+            services.AddScoped<IMembersService, MembersService>();
+
         }
     }
 }
