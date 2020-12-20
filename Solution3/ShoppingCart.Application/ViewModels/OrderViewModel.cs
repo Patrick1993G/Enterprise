b6742e14c47fa59ef55a1e7a9f3098b48293121c
Patrick1993G <49478732+Patrick1993G@users.noreply.github.com>
2020-12-20 19:04:28 +0100

    created Partial view for categories

---
+++ Solution3/ShoppingCart.Application/ViewModels/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ShoppingCart.Application.ViewModels
+{
+    class OrderViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public DateTime DatePlaced { get; set; }
+        [Required]
+        public String UserEmail { get; set; }
+    }
+}
