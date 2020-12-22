9dac100f80e9592ef6ad0e46e8d3c477e482c7ce
Patrick1993G <49478732+Patrick1993G@users.noreply.github.com>
2020-12-22 15:00:10 +0100

    Added orderDetails Controller

---
+++ Solution3/PresentationWebApp/Controllers/OrderDetailsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationWebApp.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        const string SessionKeyName = "_Basket";
+        private readonly IOrdersService _ordersService;
+        private readonly IProductsService _productsService;
+        private readonly IOrderDetailsService _orderDetailsService;
+        private IWebHostEnvironment _environment;
+        public OrderDetailsController(IOrdersService ordersService, IProductsService productsService, IOrderDetailsService orderDetailsService, IWebHostEnvironment environment)
+        {
+            _productsService = productsService;
+            _ordersService = ordersService;
+            _orderDetailsService = orderDetailsService;
+            _environment = environment;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            var list = _orderDetailsService.GetOrderDetails();
+            return View(list);
+        }
+
+        
+    }
+}
--- Solution3/ShoppingCart.Application/AutoMapper/DomainToViewModelProfile.cs
+++ Solution3/ShoppingCart.Application/AutoMapper/DomainToViewModelProfile.cs
@@ -12,7 +12,7 @@ namespace ShoppingCart.Application.AutoMapper
             CreateMap<Category, CategoryViewModel>();
             CreateMap<Member, MemberViewModel>();
             CreateMap<Order, OrderViewModel>();
-            CreateMap<OrderDetails,OrderViewModel>();
+            CreateMap<OrderDetails,OrderDetailsViewModel>();
 
         }
 
