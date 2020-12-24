c96c3a0c7d88a7e1c7297597a4d2046b96290bb3
Patrick1993G <49478732+Patrick1993G@users.noreply.github.com>
2020-12-24 13:20:48 +0100

    method for grouping the products and returning to the basket view

--- Solution3/PresentationWebApp/Controllers/BasketController.cs
+++ Solution3/PresentationWebApp/Controllers/BasketController.cs
@@ -23,32 +23,25 @@ namespace PresentationWebApp.Controllers
         public IActionResult Index()
         {
             var list = HttpContext.Session.GetString(SessionKeyName);
-            var ids = list.Split(",").ToList();
-          
+            var ids = list.Split(",");
+            var groupedIds = from id in ids
+                             group id by id into g
+                             let count = g.Count()
+                             orderby count ascending
+                             select new { value = g.Key, count = count };
             List<ProductViewModel> products = new List<ProductViewModel>();
 
             //search for duplicates
             //IEnumerable<String> duplicateIds = idList.GroupBy(x => x)
             //                        .Where(y => y.Count() > 1)
             //                        .Select(x => x.Key);
-
-            foreach (var item in ids)
+            foreach (var item in groupedIds)
             {
-                if (!String.IsNullOrEmpty(item)) // if item is not empty
+                if (!String.IsNullOrEmpty(item.value))
                 {
-                    var product = _productsService.GetProduct(new Guid(item)); // get the product by id
-                    //check if there is a same id
-                    if (product.quantity >0)
-                    {
-                        product.quantity++; // increment the quantity
-                        products.Add(product);// add it to the list
-                        int index =ids.FindIndex(idx => idx.Equals(item));
-                        ids[index] = " ";
-                    }
-                    else
-                    {
-                        products.Add(_productsService.GetProduct(new Guid(item)));
-                    }
+                    var product = _productsService.GetProduct(new Guid(item.value));
+                    product.quantity = item.count;
+                    products.Add(product);
                 }
             }
             return View(products);
